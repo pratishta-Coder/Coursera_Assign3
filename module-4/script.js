646eaba30819e76de05a155e0a45701bba766f02
pratishta-Coder <64406397+pratishta-Coder@users.noreply.github.com>
2020-05-15 13:57:51 +0530

    Add files via upload

---
+++ module-4/script.js
@@ -0,0 +1,19 @@
+
+function Practicefunc()
+{
+var a=["pranita","sakhi","jagruti","Mina"];
+for(var i=0;i<a.length;i++)
+{
+    if(a[i].charAt(0)=='j'|| a[i].charAt(0)=='J')
+    {
+       console.log("Goodbye "+a[i]);
+    }
+    else
+    {
+       console.log("Hello "+a[i]);
+     }
+}
+
+}
+
+Practicefunc();
